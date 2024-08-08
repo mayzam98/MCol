1b3f907175f355f548650c524de27203be388e93
mayzam98 <maykol9820@gmail.com>
2024-08-08 07:51:50 -0500

    Configuracion de Autenticacion

--- MCol.Web/Program.cs
+++ MCol.Web/Program.cs
@@ -1,6 +1,9 @@
 using MCol.BLL.Controller;
 using MCol.DAL.Modelo;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,6 +13,30 @@ builder.Services.AddControllersWithViews();
 //Agregar el Dbcontext al sistema de DI (Dependency Injection)
 builder.Services.AddDbContextFactory<ColegiosCOLContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("connectionCol")));
+
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            ValidIssuer = builder.Configuration["Jwt:Issuer"],
+            ValidAudience = builder.Configuration["Jwt:Audience"],
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]))
+        };
+    });
+
+// Configure authorization policies
+builder.Services.AddAuthorization(options =>
+{
+    options.AddPolicy("Admin", policy => policy.RequireRole("Admin"));
+    options.AddPolicy("User", policy => policy.RequireRole("User"));
+});
+//Register BLL Service
 builder.Services.AddScoped<UsuariosControllerBLL>();
 
 builder.Services.AddControllersWithViews();
@@ -29,6 +56,8 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
+// Use authentication and authorization
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllerRoute(
